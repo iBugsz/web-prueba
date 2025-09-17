8ee69dec290ff3a58613ecd88bedfefdb206b10f
Nelson-hast <ngarciab357@gmail.com>
2025-09-16 20:25:22 -0500

    solo lee los nombres

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const apiData = document.getElementById('apiData');
 
 btn.addEventListener('click', () => {
   fetch(
-    'https://raw.githubusercontent.com/samp-comunity/plujin-manager/refs/heads/main/assets/scripts.json',
+    'https://raw.githubusercontent.com/samp-comunity/plujin-manager/main/assets/scripts.json',
   )
     .then((res) => {
       if (!res.ok) throw new Error('No se pudo cargar el JSON');
       return res.json();
     })
     .then((data) => {
-      // Mostrar los datos en el <pre>
-      apiData.textContent = JSON.stringify(data, null, 2);
+      // Extraer todos los nombres
+      const names = data.map((item) => item.name);
+      // Mostrar separados por coma
+      apiData.textContent = names.join(', ');
     })
     .catch((err) => {
       apiData.textContent = 'Error al cargar la API: ' + err;
