809fbc5d18338c3a0d5e636a203b7aa131b4689e
Nelson-hast <ngarciab357@gmail.com>
2025-09-16 20:22:36 -0500

    fix script.js

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ btn.addEventListener('click', () => {
   fetch(
     'https://raw.githubusercontent.com/samp-comunity/plujin-manager/refs/heads/main/assets/scripts.json',
   )
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) throw new Error('No se pudo cargar el JSON');
+      return res.json();
+    })
     .then((data) => {
+      // Mostrar los datos en el <pre>
       apiData.textContent = JSON.stringify(data, null, 2);
     })
     .catch((err) => {
